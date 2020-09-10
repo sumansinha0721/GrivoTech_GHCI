e37a30fa4d1bafd064948b9da8453a2bd034669e
Muskan645gupta <51221607+Muskan645gupta@users.noreply.github.com>
2020-09-10 11:45:12 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-    $db = mysqli_connect("localhost","root","","sihh");
+    $db = mysqli_connect("localhost","root","","sihh(8)");
     if (mysqli_connect_errno()){
         echo "server not connected" . mysqli_connect_error();
     }
